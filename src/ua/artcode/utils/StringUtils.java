d35b5c7a1135029f2d62b930cb0668b9ce274123
presly808 <presly808@gmail.com>
2017-01-22 12:20:11 +0200

    init commit

---
+++ src/ua/artcode/utils/StringUtils.java
@@ -0,0 +1,35 @@
+package ua.artcode.utils;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author Serhii Bilobrov
+ * @since 1.7
+ */
+public final class StringUtils {
+
+    private static final String SEPARATOR = ",";
+
+    private StringUtils() {}
+
+    //     12,34,54,56,67,878,98
+    public static List<Integer> getIntArr(String src){
+
+        if(src == null || src.isEmpty()){
+            throw new NullPointerException();
+        }
+
+        String trimmed = src.trim();
+
+        String[] numbersStr = trimmed.split(SEPARATOR);
+
+        List<Integer> list =
+                Arrays.stream(numbersStr).map(Integer::parseInt).collect(Collectors.toList());
+
+        return list;
+
+    }
+
+}
