c2c2ac3225efef484192bf1cbd35317055cd25d6
Vitalii <w1lat@ukr.net>
2017-01-30 22:54:55 +0200

    patterns of projecting(factory, factory method) add

---
+++ balitskiyVitalii/src/Week2/Homework/Factory1.java
@@ -0,0 +1,81 @@
+package Week2.Homework;
+
+/**
+ * Created by vitalii on 30.01.17.
+ */
+public class Factory1 {
+
+    public static void main(String[] args) {
+
+        Factory factory = new SamsungFactory();
+        Radio radio = factory.getRadio();
+        Telephone phone = factory.getPhone();
+        TVSet televizor = factory.getTV();
+
+        radio.turnOn();
+        televizor.turnOn();
+        phone.ring();
+    }
+
+
+
+}
+
+interface TVSet{
+    void turnOn();
+}
+
+interface Radio{
+    void turnOn();
+}
+
+interface Telephone{
+    void ring();
+        }
+
+class SamsungTV implements TVSet{
+
+    @Override
+    public void turnOn() {
+        System.out.println("Dobrui ve4er druzya @SAMSUNG");
+    }
+}
+class SamsungRadio implements Radio{
+
+    @Override
+    public void turnOn() {
+        System.out.println("Kyiv na zvyzku @SAMSUNG");
+    }
+}
+
+class SamsungPhone implements Telephone{
+
+    @Override
+    public void ring() {
+        System.out.println("Vizmit sluhavku @SAMSUNG");
+    }
+}
+
+interface Factory{
+    TVSet getTV();
+    Radio getRadio();
+    Telephone getPhone();
+}
+
+class SamsungFactory implements Factory{
+
+    @Override
+    public TVSet getTV() {
+        return new SamsungTV();
+    }
+
+    @Override
+    public Radio getRadio() {
+        return new SamsungRadio();
+    }
+
+    @Override
+    public Telephone getPhone() {
+        return new SamsungPhone();
+    }
+}
---
+++ balitskiyVitalii/src/Week2/Homework/Factory2.java
@@ -0,0 +1,7 @@
+package Week2.Homework;
+
+/**
+ * Created by vitalii on 30.01.17.
+ */
+public class Factory2 {
+}
