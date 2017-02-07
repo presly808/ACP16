f33582aceb098a05d5e5cf1f3a3d3ab23ec79dad
Sasha943 <MaslikSASHA1@gmail.com>
2017-02-07 23:23:25 +0200

    add idto

---
+++ MaslovskyjOleksandr/src/main/java/com/yandex/controllers/IDto.java
@@ -0,0 +1,20 @@
+package com.yandex.controllers;
+
+import com.yandex.exception.NoOrderFoundException;
+import com.yandex.models.Order;
+import com.yandex.models.Taxi;
+import yandex.exception.NoTaxiFoundException;
+
+import java.util.UUID;
+
+public interface IDto {
+
+    boolean saveOrderToDB(Order order);
+
+    boolean removeOrderFromDb(UUID id) throws NoOrderFoundException;
+
+    boolean saveTaxiToDb(Taxi taxi);
+
+    boolean removeTaxiFromDB(UUID id) throws NoTaxiFoundException;
+
+}
---
+++ MaslovskyjOleksandr/src/main/java/yandex/exception/NoTaxiFoundException.java
@@ -0,0 +1,11 @@
+package yandex.exception;
+
+/**
+ * Created by ubuntu on 07.02.17.
+ */
+public class NoTaxiFoundException extends Exception {
+
+    public NoTaxiFoundException() {
+        super("Taxi not found");
+    }
+}
