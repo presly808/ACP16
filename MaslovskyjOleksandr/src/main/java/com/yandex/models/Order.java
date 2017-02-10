503b7bef8edba8aa76a4ca67a7b0ae4a029bd2e1
Sasha943 <MaslikSASHA1@gmail.com>
2017-02-10 12:20:46 +0200

    add date

--- MaslovskyjOleksandr/src/main/java/com/yandex/models/Order.java
+++ MaslovskyjOleksandr/src/main/java/com/yandex/models/Order.java
@@ -1,8 +1,10 @@
 package com.yandex.models;
 
 import com.yandex.controllers.ILocation;
+import com.yandex.utils.DateGenerator;
 import com.yandex.utils.IdGenerator;
 
+import javax.xml.crypto.Data;
 import java.util.UUID;
 
 /**
@@ -15,6 +17,7 @@ public class Order {
     private Client client;
     private double price;
     private ILocation location;
+    private String orderData;
 
     public Order(Taxi taxi, Client client, double price, ILocation location) {
         this.taxi = taxi;
@@ -22,6 +25,7 @@ public class Order {
         this.price = price;
         this.location = location;
         this.id = IdGenerator.generateId();
+        this.orderData = DateGenerator.getCurrentDate();
     }
 
     public UUID getId() {
