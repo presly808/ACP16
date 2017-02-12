689bdeef0561f1ac9593d93ea87905123ddd6351
presly808 <presly808@gmail.com>
2017-02-12 10:46:15 +0200

    server as observer

--- src/ua/artcode/week3/client_server/MyServer.java
+++ src/ua/artcode/week3/client_server/MyServer.java
@@ -3,6 +3,7 @@ package ua.artcode.week3.client_server;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -13,10 +14,12 @@ import java.util.List;
  */
 public class MyServer {
 
-    private static List<Socket> clients = new ArrayList<>();
-
+    // logging(sout),
+    // send message as json(use GSon lib)
+    // new thread that accepts messages from client then notifyAll subscribers
     public static void main(String[] args) {
 
+        InputMessagesObserver inputMessagesObserver = new InputMessagesObserver();
         //192.168.1.115
         try {
             ServerSocket serverSocket = new ServerSocket(8888);
@@ -24,10 +27,10 @@ public class MyServer {
 
             //asynch action
             new Thread(() -> {
-                while(true){
+                while (true) {
                     try {
-                        Socket client =  serverSocket.accept();
-                        clients.add(client);
+                        Socket client = serverSocket.accept();
+                        inputMessagesObserver.subscribe(new SocketClient(client));
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
@@ -42,17 +45,10 @@ public class MyServer {
         // separate action
 
 
+        new Thread(() -> {
+            while (true) {
 
-
-        while(true){
-            try {
-
-                for (Socket client : clients) {
-                    PrintWriter output = new PrintWriter(client.getOutputStream());
-                    String response = String.format("CurrentTime %tc", new Date())  ;
-                    output.println(response);
-                    output.flush();
-                }
+                inputMessagesObserver.notifyAllSubs(new MyMessage("server",LocalDateTime.now(),"hello"));
 
                 try {
                     Thread.sleep(2000);
@@ -60,13 +56,94 @@ public class MyServer {
                     e.printStackTrace();
                 }
 
-            } catch (IOException e) {
-                e.printStackTrace();
             }
+        }).start();
 
 
-        }
+    }
+}
+
+class InputMessagesObserver {
+
+    private List<SocketClient> subscribers;
+
+    public InputMessagesObserver() {
+        subscribers = new ArrayList<>();
+    }
+
+    public void subscribe(SocketClient socketClient){
+        subscribers.add(socketClient);
+    }
+
+    public void notifyAllSubs(MyMessage message){
+
+        subscribers.stream()
+                .filter((sub) -> !sub.getIp().equals(message.getFrom()))
+                .forEach((sub) -> sub.sendMessage(message));
+
+    }
 
 
+}
+
+class MyMessage {
+
+    private String from;
+    private LocalDateTime creationTime;
+    private String body;
+
+    public MyMessage(String from, LocalDateTime creationTime, String body) {
+        this.from = from;
+        this.creationTime = creationTime;
+        this.body = body;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public LocalDateTime getCreationTime() {
+        return creationTime;
+    }
+
+    public String getBody() {
+        return body;
+    }
+}
+
+
+class SocketClient {
+
+    private String ip;
+    private Socket socket;
+    private PrintWriter printWriter;
+    private BufferedReader bufferedReader;
+
+    public SocketClient(Socket socket) throws IOException {
+        this.socket = socket;
+
+        this.ip = socket.getInetAddress().toString();
+        this.bufferedReader = new BufferedReader(
+                new InputStreamReader(socket.getInputStream()));
+
+        this.printWriter = new PrintWriter(socket.getOutputStream());
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public PrintWriter getPrintWriter() {
+        return printWriter;
+    }
+
+    public BufferedReader getBufferedReader() {
+        return bufferedReader;
+    }
+
+    public void sendMessage(MyMessage message) {
+        printWriter.printf("Time %s, From %s, Message %s \n",
+                message.getCreationTime().toString(), message.getFrom(), message.getBody());
+        printWriter.flush();
     }
 }
