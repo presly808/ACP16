c6f0a0fd79ca62a2ed9241acdb8c46dd6b0a823a
Sasha943 <MaslikSASHA1@gmail.com>
2017-02-25 10:14:29 +0200

    fix test

--- MaslovskyjOleksandr/src/main/java/com/bank/bank/test/UnsynchBankTest.java
+++ MaslovskyjOleksandr/src/main/java/com/bank/bank/test/UnsynchBankTest.java
@@ -45,10 +45,7 @@ public class UnsynchBankTest {
             thread.start();
         }
 
-        double actualBalance = 0;
-        for (double account : bank.getAccounts()) {
-            actualBalance = bank.getTotalBalance();
-        }
+        double actualBalance = bank.getTotalBalance();
 
         Assert.assertTrue(expectedBalance == actualBalance);
     }
