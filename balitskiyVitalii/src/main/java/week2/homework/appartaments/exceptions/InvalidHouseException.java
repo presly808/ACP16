99b470e8cdffa6bfd4f09388bc32d961690b8e2d
w1lat <w1lat@ukr.net>
2017-02-26 09:06:34 +0200

    JDBC testing added

---
+++ balitskiyVitalii/src/main/java/week2/homework/appartaments/exceptions/InvalidHouseException.java
@@ -0,0 +1,10 @@
+package week2.homework.appartaments.exceptions;
+
+/**
+ * Created by vitalii on 01.02.17.
+ */
+public class InvalidHouseException extends Exception {
+    public InvalidHouseException(String message){
+        super(message);
+    }
+}
---
+++ balitskiyVitalii/src/main/java/week2/homework/appartaments/model/Accessory.java
@@ -0,0 +1,7 @@
+package week2.homework.appartaments.model;
+
+/**
+ * Created by vitalii on 01.02.17.
+ */
+public class Accessory {
+}
